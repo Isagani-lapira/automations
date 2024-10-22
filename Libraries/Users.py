0dc4f42662b0ecd2c05f7a4f2891680c9bcc5334
isagani-lapira <lapiraisagani@gmail.com>
2024-10-23 07:17:21 +0800

    set static state

--- Libraries/Users.py
+++ Libraries/Users.py
@@ -1,9 +1,11 @@
 import requests
 import string
 import secrets
+import random
 class Users():
     def get_users_via_api(self):
         response = requests.get(url="https://jsonplaceholder.typicode.com/users",verify=False)
+        print(response)
         return response.json()
     
     def generate_password(self):
@@ -11,6 +13,57 @@ class Users():
         return ''.join(secrets.choice(alphabet) for i in range(20))
     
     def get_random_word(self):
-        response = requests.get( "https://random-word-api.herokuapp.com/word",verify=False)
+        states = [
+                "Alabama",
+                "Alaska",
+                "Arizona",
+                "Arkansas",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Iowa",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Maine",
+                "Maryland",
+                "Massachusetts",
+                "Michigan",
+                "Minnesota",
+                "Mississippi",
+                "Missouri",
+                "Montana",
+                "Nebraska",
+                "Nevada",
+                "New Hampshire",
+                "New Jersey",
+                "New Mexico",
+                "New York",
+                "North Carolina",
+                "North Dakota",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "Rhode Island",
+                "South Carolina",
+                "South Dakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Vermont",
+                "Virginia",
+                "Washington",
+                "West Virginia",
+                "Wisconsin",
+                "Wyoming"
+            ]
 
-        return response.json()[0].title()
+        return random.choice(states)
